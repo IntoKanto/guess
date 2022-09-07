1b17eb8d4e224a73f0551d20243143199ec65b2e
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-09-07 15:28:16 +0300

    guess tehtävä

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [guess, setGuess] = useState('');
+  const [kertoja, setKertoja] = useState(1);
+  const [num, setNum] = useState(Math.floor(Math.random() * 100) +1);
+  const [teksti ,setTeksti] = useState('Guess a number');
+
+  
+  
+
+  const arvaus = () => {
+
+    if (guess > num) {
+      setTeksti('Your guess is too high')
+      setKertoja(kertoja + 1)
+    }
+    else if (guess < num) {
+      setTeksti('Your guess is too low')
+      setKertoja(kertoja + 1)
+    }
+    else {
+      Alert.alert('You guessed the number in ' + kertoja + ' guesses')
+      setTeksti('Guess a number');
+      setKertoja(1);
+      setNum(Math.floor(Math.random() * 100) +1)
+    }
+    setGuess('');
+    
+    
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Text>{teksti}</Text>
+     <TextInput
+     style={styles.input}
+     value={guess}
+     onChangeText={guess => setGuess(guess)}
+     keyboardType='numeric'
+     /> 
+     <Button
+     title='Make a guess' 
+     onPress={arvaus}
+     />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +58,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 50,
+    borderColor: 'blue',
+    borderWidth: 1
+  },
+  buttons: {
+    justifyContent: 'flex-start'
   },
 });
